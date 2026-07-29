21bb66052bf59d6e2848d73a8c42a37867a6562c
IbraDevWeb <nishimiya.ichida@gmail.com>
2026-07-29 14:45:21 +0200

    Add offline app shell caching

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+const CACHE_VERSION = 'athar-pro-v1';
+const APP_SHELL = [
+    './',
+    './index.html',
+    './manifest.json',
+    './css/style.css',
+    './js/config.js',
+    './js/app.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_VERSION)
+            .then((cache) => Promise.allSettled(APP_SHELL.map((url) => cache.add(url))))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_VERSION).map((key) => caches.delete(key))))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    const request = event.request;
+    if (request.method !== 'GET') return;
+
+    const url = new URL(request.url);
+    if (url.origin !== self.location.origin) return;
+
+    if (request.mode === 'navigate') {
+        event.respondWith(
+            fetch(request)
+                .then((response) => {
+                    const copy = response.clone();
+                    caches.open(CACHE_VERSION).then((cache) => cache.put('./index.html', copy));
+                    return response;
+                })
+                .catch(() => caches.match('./index.html'))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(request).then((cached) => {
+            const network = fetch(request)
+                .then((response) => {
+                    if (response.ok) {
+                        const copy = response.clone();
+                        caches.open(CACHE_VERSION).then((cache) => cache.put(request, copy));
+                    }
+                    return response;
+                })
+                .catch(() => cached);
+            return cached || network;
+        })
+    );
+});
